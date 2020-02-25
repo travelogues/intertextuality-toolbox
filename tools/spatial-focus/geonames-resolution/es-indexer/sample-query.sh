619192bd5ba9711ce5245db03a3d6f43dbc82281
Rainer Simon <rainer.simon@ait.ac.at>
2020-02-25 12:05:47 +0100

    Cleanup

--- tools/spatial-focus/geonames-resolution/es-indexer/sample-query.sh
+++ tools/spatial-focus/geonames-resolution/es-indexer/sample-query.sh
@@ -6,7 +6,6 @@ curl -XPOST "http://localhost:9200/geonames/_search?pretty" -H "Content-Type: ap
       ],
       "should" : [
         { "term" : { "properties.title" : "wien" } },
-        { "term" : { "properties.title" : "wien" } },
         { "match" : { "properties.feature_class" : { "query" : "P", "boost" : 100000 } } },
         { "function_score" : { "field_value_factor": { "field" : "properties.population", "factor" : 0.001 } } }
       ]
