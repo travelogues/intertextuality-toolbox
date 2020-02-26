298ecc6259cef5721ce0e8bcefe7dc5acf41dc20
Rainer Simon <rainer.simon@ait.ac.at>
2020-02-26 14:23:35 +0100

    Added ngram similarity results

--- tools/text-reuse/compute-similarities.py
+++ tools/text-reuse/compute-similarities.py
@@ -7,14 +7,14 @@ the given input folder and creates a CSV file that
 is compatible with Gephi.
 '''
 
-INPUT_FOLDER = '../../sample-data/near-duplicates'
+INPUT_FOLDER = '../../sample-data/two-related'
 
 # Length of the n-grams to use as the basis
 # Recommended: 8
 NGRAM_SIZE = 8
 
 # Write output CSV to this file
-OUTPUT_FILE = '../../results/near-duplicates/similarities_ngram.csv'
+OUTPUT_FILE = '../../results/two-related/similarities_ngram.csv'
 
 files = list_files(INPUT_FOLDER)
 print(f'Got {len(files)} files')
