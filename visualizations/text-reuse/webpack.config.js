bc685d778f76daecd83f0c7feb02aa7513ecce52
Rainer Simon <rainer.simon@ait.ac.at>
2020-04-22 14:03:45 +0200

    Added project skeleton for intertextuality vis

---
+++ visualizations/text-reuse/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const fs = require('fs');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const APP_DIR = fs.realpathSync(process.cwd());
+
+const resolveAppPath = relativePath => path.resolve(APP_DIR, relativePath);
+
+module.exports = {
+  entry: resolveAppPath('src'),
+  output: {
+    filename: 'text-reuse-vis.min.js'
+  },
+  performance: {
+    hints: false
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/, 
+        use: { 
+          loader: 'babel-loader' ,
+          options: {
+            "presets": [
+              "@babel/preset-env"
+            ],
+            "plugins": [
+              [
+                "@babel/plugin-proposal-class-properties"
+              ]
+            ]
+          }
+        }
+      },
+      { test: /\.css$/,  use: [ 'style-loader', 'css-loader'] },
+      { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] }
+    ]
+  },
+  devServer: {
+    contentBase: resolveAppPath('public'),
+    compress: true,
+    hot: true,
+    host: process.env.HOST || 'localhost',
+    port: 3000,
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: resolveAppPath('public/index.html')
+    })
+  ]
+}
